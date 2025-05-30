05161dafd2c2e81f3af652be1396ded2312ffc9d
Luca Rinaldi <lucarin@protonmail.com>
2025-05-30 09:55:59 +0200

    fix: wrong tmpdir path

--- debian/install-common.sh
+++ debian/install-common.sh
@@ -5,3 +5,7 @@ set -xe
 adb shell apt-get update
 adb shell apt-get install -y tree curl
 
+# Set TMPDIR for all users
+adb shell sh -c "cat > /etc/profile.d/50-tmpdir.sh" <<EOF
+export TMPDIR=/tmp
+EOF
