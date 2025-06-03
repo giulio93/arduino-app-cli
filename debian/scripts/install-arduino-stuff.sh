7564ca9fc860ec7b56816e5815fde30d2381bc49
Alessio Perugini <alessio@perugini.xyz>
2025-06-03 14:10:06 +0200

    RB1: add install-arduino-repo.sh script
    Co-authored-by: lucarin91 <lucarin@protonmail.com>

---
+++ debian/scripts/install-arduino-stuff.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+adb shell sh -c 'cat > /etc/apt/sources.list.d/arduino.list' <<EOF
+deb [trusted=yes] https://apt-repo.arduino.cc stable main
+EOF
+
+adb shell sh -c 'cat > /etc/apt/auth.conf.d/arduino.conf' <<EOF
+machine apt-repo.arduino.cc
+login arduino
+password aptexperiment
+EOF
+
+adb shell apt-get update
+adb shell apt-get install -y arduino-orchestrator arduino-router
