4c1cde6d158c67b859659873641455ce2f9e74ae
Alessio Perugini <alessio@perugini.xyz>
2025-07-17 12:40:49 +0200

    board: skip slow test in CI

--- pkg/board/board_test.go
+++ pkg/board/board_test.go
@@ -1,12 +1,17 @@
 package board
 
 import (
+	"os"
 	"testing"
 
 	"github.com/stretchr/testify/require"
 )
 
 func TestEnsurePlatformInstalled(t *testing.T) {
+	// We skip it in CI, as downloading andinstalling the core takes ~6 minutes
+	if os.Getenv("CI") != "" {
+		t.Skip("Skipping slow test")
+	}
 	// Example test function
 	err := EnsurePlatformInstalled(t.Context(), "arduino:zephyr:unoq")
 	require.NoError(t, err)
