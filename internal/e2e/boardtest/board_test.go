e98008f436228c2917dfda4bdb32c0a088477473
Giulio Pilotto <pilotto.giulio@gmail.com>
2025-11-16 18:25:29 +0100

    test example blink shutdown

--- internal/e2e/boardtest/board_test.go
+++ internal/e2e/boardtest/board_test.go
@@ -11,70 +11,30 @@ import (
 )
 
 const daemonHost = "127.0.0.1:8800"
+const sseAppEventsPath = "http://127.0.0.1:8800/v1/apps/events"
+const sseAppIDEventsPath = "http://127.0.0.1:8800/v1/apps/%s/events"
 
 func TestBlinkBoard(t *testing.T) {
 
-	t.Cleanup(func() { exec.Command("arduino-app-cli", "app", "stop", "examples:blink").Run() })
+	//t.Cleanup(func() { exec.Command("arduino-app-cli", "app", "stop", "examples:blink").Run() })
 	waitForPort(t, daemonHost, 5*time.Second)
 
-	cmd := exec.Command("arduino-app-cli", "app", "start", "examples:blink")
-	output, err := cmd.CombinedOutput()
-	if err != nil {
-		t.Fatalf("Failed to start Blink example: %v\nOutput: %s", err, string(output))
-	}
-	fmt.Println("start output", string(output))
+	exec.Command("arduino-app-cli", "app", "start", "examples:blink").Run()
 
-	passed := waitForAppToStart(t, daemonHost, "running")
-	require.True(t, passed, "Blink app reach running state")
-	time.Sleep(3 * time.Second)
-
-	cmd = exec.Command("arduino-app-cli", "app", "stop", "examples:blink-with-ui")
-	output, err = cmd.CombinedOutput()
-	if err != nil {
-		t.Fatalf("Failed to stop Blink example: %v\nOutput: %s", err, string(output))
-	}
-	fmt.Println("stop output", string(output))
-
-	passed = waitForAppToStart(t, daemonHost, "running")
+	passed := waitForSSEevent(t, sseAppEventsPath, "Blink LED", "running")
 	require.True(t, passed, "Blink app reach running state")
 
-	cmd = exec.Command("arduino-app-cli", "app", "stop", "examples:blink")
-	output, err = cmd.CombinedOutput()
-	if err != nil {
-		t.Fatalf("Failed to stop Blink example: %v\nOutput: %s", err, string(output))
-	}
-	fmt.Println("stop output", string(output))
+	exec.Command("arduino-app-cli", "app", "stop", "examples:blink").Run()
 
-	passed = waitForAppToStart(t, daemonHost, "stopped")
-	require.True(t, passed, "Blink app reach running state")
-
-	cmd = exec.Command("arduino-app-cli", "app", "start", "examples:blink-with-ui")
-	output, err = cmd.CombinedOutput()
-	if err != nil {
-		t.Fatalf("Failed to start Blink example: %v\nOutput: %s", err, string(output))
-	}
-	fmt.Println("start output", string(output))
-	passed = waitForAppToStart(t, daemonHost, "running")
-	require.True(t, passed, "Blink app reach running state")
-
-	cmd = exec.Command("arduino-app-cli", "app", "stop", "examples:blink-with-ui")
-	output, err = cmd.CombinedOutput()
-	if err != nil {
-		t.Fatalf("Failed to stop Blink example: %v\nOutput: %s", err, string(output))
-	}
-	fmt.Println("stop output", string(output))
-
-	passed = waitForAppToStart(t, daemonHost, "stopped")
+	passed = waitForSSEevent(t, sseAppEventsPath, "Blink LED", "stopped")
 	require.True(t, passed, "Blink app reach running state")
 
 }
 
-func waitForAppToStart(t *testing.T, host string, status string) bool {
+func waitForSSEevent(t *testing.T, url, name, status string) bool {
 	t.Helper()
 
 	appo := false
-
-	url := fmt.Sprintf("http://%s/v1/apps/events", host)
 
 	itr := NewSSEClient(t.Context(), "GET", url)
 	for event, err := range itr {
@@ -83,7 +43,7 @@ func waitForAppToStart(t *testing.T, host string, status string) bool {
 			fmt.Println("Error receiving event:", err)
 		}
 		t.Logf("Received event: ID=%s, Event=%s, Data=%s\n", event.ID, event.Event, string(event.Data))
-		if event.Event == "app" && event.Data != nil && strings.Contains(string(event.Data), fmt.Sprintf(`"status":"%s"`, status)) {
+		if event.Event == "app" && event.Data != nil && strings.Contains(string(event.Data), fmt.Sprintf(`"status":"%s"`, status)) && strings.Contains(string(event.Data), fmt.Sprintf(`"name":"%s"`, name)) {
 			appo = true
 			break
 		}
