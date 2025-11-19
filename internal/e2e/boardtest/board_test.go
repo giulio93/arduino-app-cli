9c5c5af4d48ef91ff63fc694a98a83015ab70a5e
Giulio Pilotto <pilotto.giulio@gmail.com>
2025-11-19 17:35:37 +0100

    using  go script file

--- internal/e2e/boardtest/board_test.go
+++ internal/e2e/boardtest/board_test.go
@@ -2,53 +2,25 @@ package boardtest
 
 import (
 	"fmt"
-	"os/exec"
-	"strings"
+	"runtime"
 	"testing"
-	"time"
-
-	"github.com/stretchr/testify/require"
 )
 
 const daemonHost = "127.0.0.1:8800"
-const sseAppEventsPath = "http://127.0.0.1:8800/v1/apps/events"
-const sseAppIDEventsPath = "http://127.0.0.1:8800/v1/apps/%s/events"
+
+// const sseAppEventsPath = "http://127.0.0.1:8800/v1/apps/events"
+// const sseAppIDEventsPath = "http://127.0.0.1:8800/v1/apps/%s/events"
+
+var arch = runtime.GOARCH
 
 func TestBlinkBoard(t *testing.T) {
 
-	//t.Cleanup(func() { exec.Command("arduino-app-cli", "app", "stop", "examples:blink").Run() })
-	waitForPort(t, daemonHost, 5*time.Second)
+	tagAppCli := fetchDebPackageLatest(t, "build/stable", "arduino-app-cli")
+	fetchDebPackageLatest(t, "build/stable", "arduino-router")
+	majorTag := genMajorTag(t, tagAppCli)
 
-	exec.Command("arduino-app-cli", "app", "start", "examples:blink").Run()
-
-	passed := waitForSSEevent(t, sseAppEventsPath, "Blink LED", "running")
-	require.True(t, passed, "Blink app reach running state")
-
-	exec.Command("arduino-app-cli", "app", "stop", "examples:blink").Run()
-
-	passed = waitForSSEevent(t, sseAppEventsPath, "Blink LED", "stopped")
-	require.True(t, passed, "Blink app reach running state")
-
-}
-
-func waitForSSEevent(t *testing.T, url, name, status string) bool {
-	t.Helper()
-
-	appo := false
-
-	itr := NewSSEClient(t.Context(), "GET", url)
-	for event, err := range itr {
-		if err != nil {
-			require.NoError(t, err)
-			fmt.Println("Error receiving event:", err)
-		}
-		t.Logf("Received event: ID=%s, Event=%s, Data=%s\n", event.ID, event.Event, string(event.Data))
-		if event.Event == "app" && event.Data != nil && strings.Contains(string(event.Data), fmt.Sprintf(`"status":"%s"`, status)) && strings.Contains(string(event.Data), fmt.Sprintf(`"name":"%s"`, name)) {
-			appo = true
-			break
-		}
-	}
-
-	return appo
+	fmt.Printf("Updating from stable version %s to unstable version %s \n", tagAppCli, majorTag)
+	fmt.Printf("Building local deb version %s \n", majorTag)
+	buildDebVersion(t, "build", majorTag, arch)
 
 }
--- internal/e2e/boardtest/helpers.go
+++ internal/e2e/boardtest/helpers.go
@@ -2,15 +2,247 @@ package boardtest
 
 import (
 	"bufio"
+	"bytes"
 	"context"
+	"encoding/json"
 	"fmt"
 	"iter"
+	"log"
 	"net"
 	"net/http"
+	"os"
+	"os/exec"
+	"path/filepath"
+	"strconv"
 	"strings"
 	"testing"
 	"time"
+
+	"github.com/stretchr/testify/require"
 )
+
+func fetchDebPackageLatest(t *testing.T, path, repo string) string {
+	t.Helper()
+
+	repo = fmt.Sprintf("github.com/arduino/%s", repo)
+	cmd := exec.Command(
+		"gh", "release", "list",
+		"--repo", repo,
+		"--exclude-pre-releases",
+		"--limit", "1",
+	)
+
+	output, err := cmd.CombinedOutput()
+	if err != nil {
+		log.Fatalf("command failed: %v\nOutput: %s", err, output)
+	}
+
+	fmt.Println(string(output))
+
+	fields := strings.Fields(string(output))
+	if len(fields) == 0 {
+		log.Fatal("could not parse tag from gh release list output")
+	}
+	tag := fields[0]
+
+	fmt.Println("Detected tag:", tag)
+	cmd2 := exec.Command(
+		"gh", "release", "download",
+		tag,
+		"--repo", repo,
+		"--pattern", "*.deb",
+		"--dir", path,
+	)
+
+	out, err := cmd2.CombinedOutput()
+	if err != nil {
+		log.Fatalf("download failed: %v\nOutput: %s", err, out)
+	}
+
+	return tag
+
+}
+
+func buildDebVersion(t *testing.T, storePath, tagVersion, arch string) {
+	t.Helper()
+	cwd, err := os.Getwd()
+	if err != nil {
+		panic(err)
+	}
+	outputDir := filepath.Join(cwd, storePath)
+
+	tagVersion = fmt.Sprintf("VERSION=%s", tagVersion)
+	arch = fmt.Sprintf("ARCH=%s", arch)
+	outputDir = fmt.Sprintf("OUTPUT=%s", outputDir)
+
+	cmd := exec.Command(
+		"go", "tool", "task", "build-deb",
+		tagVersion,
+		arch,
+		outputDir,
+	)
+
+	if err := cmd.Run(); err != nil {
+		log.Fatalf("failed to run build command: %v", err)
+	}
+}
+
+func genMajorTag(t *testing.T, tag string) string {
+	t.Helper()
+
+	parts := strings.Split(tag, ".")
+	last := parts[len(parts)-1]
+
+	lastNum, _ := strconv.Atoi(strings.TrimPrefix(last, "v"))
+	lastNum++
+
+	parts[len(parts)-1] = strconv.Itoa(lastNum)
+	newTag := strings.Join(parts, ".")
+
+	return newTag
+}
+
+func genMinorTag(t *testing.T, tag string) string {
+	t.Helper()
+
+	parts := strings.Split(tag, ".")
+	last := parts[len(parts)-1]
+
+	lastNum, _ := strconv.Atoi(strings.TrimPrefix(last, "v"))
+	if lastNum > 0 {
+		lastNum--
+	}
+
+	parts[len(parts)-1] = strconv.Itoa(lastNum)
+	newTag := strings.Join(parts, ".")
+
+	if !strings.HasPrefix(newTag, "v") {
+		newTag = "v" + newTag
+	}
+	return newTag
+}
+
+func buildDockerImage(t *testing.T, dockerfile, name, arch string) {
+	t.Helper()
+
+	arch = fmt.Sprintf("ARCH=%s", arch)
+
+	cmd := exec.Command("docker", "build", "--build-arg", arch, "-t", name, "-f", dockerfile, ".")
+	// Capture both stdout and stderr
+	var out bytes.Buffer
+	var stderr bytes.Buffer
+	cmd.Stdout = &out
+	cmd.Stderr = &stderr
+
+	err := cmd.Run()
+	if err != nil {
+		fmt.Printf("❌ Docker build failed: %v\n", err)
+		fmt.Printf("---- STDERR ----\n%s\n", stderr.String())
+		fmt.Printf("---- STDOUT ----\n%s\n", out.String())
+		return
+	}
+
+	fmt.Println("✅ Docker build succeeded!")
+}
+
+func startDockerContainer(t *testing.T, containerName string, containerImageName string) {
+	t.Helper()
+
+	cmd := exec.Command(
+		"docker", "run", "--rm", "-d",
+		"-p", "8800:8800",
+		"--privileged",
+		"--cgroupns=host",
+		"--network", "host",
+		"-v", "/sys/fs/cgroup:/sys/fs/cgroup:rw",
+		"-v", "/var/run/docker.sock:/var/run/docker.sock",
+		"-e", "DOCKER_HOST=unix:///var/run/docker.sock",
+		"--name", containerName,
+		containerImageName,
+	)
+
+	if err := cmd.Run(); err != nil {
+		t.Fatalf("failed to run container: %v", err)
+	}
+
+}
+
+func getAppCliVersion(t *testing.T, containerName string) string {
+	t.Helper()
+
+	cmd := exec.Command(
+		"docker", "exec",
+		"--user", "arduino",
+		containerName,
+		"arduino-app-cli", "version", "--format", "json",
+	)
+	output, err := cmd.CombinedOutput()
+	if err != nil {
+		log.Fatalf("command failed: %v\nOutput: %s", err, output)
+	}
+
+	var version struct {
+		Version       string `json:"version"`
+		DaemonVersion string `json:"daemon_version"`
+	}
+	err = json.Unmarshal(output, &version)
+	require.NoError(t, err)
+	// TODO to enable after 0.6.7
+	// require.Equal(t, version.Version, version.DaemonVersion, "client and daemon versions should match")
+	require.NotEmpty(t, version.Version)
+	return version.Version
+
+}
+
+func runSystemUpdate(t *testing.T, containerName string) {
+	t.Helper()
+
+	cmd := exec.Command(
+		"docker", "exec",
+		"--user", "arduino",
+		containerName,
+		"arduino-app-cli", "system", "update", "--yes",
+	)
+	output, err := cmd.CombinedOutput()
+	require.NoError(t, err, "system update failed: %s", output)
+	t.Logf("system update output: %s", output)
+}
+
+func stopDockerContainer(t *testing.T, containerName string) {
+	t.Helper()
+
+	cleanupCmd := exec.Command("docker", "rm", "-f", containerName)
+
+	fmt.Println("🧹 Removing Docker container " + containerName)
+	if err := cleanupCmd.Run(); err != nil {
+		fmt.Printf("⚠️  Warning: could not remove container (might not exist): %v\n", err)
+	}
+
+}
+
+func putUpdateRequest(t *testing.T, host string) {
+
+	t.Helper()
+
+	url := fmt.Sprintf("http://%s/v1/system/update/apply", host)
+
+	req, err := http.NewRequest(http.MethodPut, url, nil)
+	if err != nil {
+		log.Fatalf("Error creating request: %v", err)
+	}
+
+	req.Header.Set("Content-Type", "application/json")
+
+	client := &http.Client{}
+	resp, err := client.Do(req)
+	if err != nil {
+		log.Fatalf("Error sending request: %v", err)
+	}
+	defer resp.Body.Close()
+
+	require.Equal(t, 202, resp.StatusCode)
+
+}
 
 func NewSSEClient(ctx context.Context, method, url string) iter.Seq2[Event, error] {
 	return func(yield func(Event, error) bool) {
@@ -80,4 +312,42 @@ func waitForPort(t *testing.T, host string, timeout time.Duration) { // nolint:u
 		time.Sleep(200 * time.Millisecond)
 	}
 	t.Fatalf("Server at %s did not start within %v", host, timeout)
+}
+
+func waitForUpgrade(t *testing.T, host string) {
+	t.Helper()
+
+	url := fmt.Sprintf("http://%s/v1/system/update/events", host)
+
+	itr := NewSSEClient(t.Context(), "GET", url)
+	for event, err := range itr {
+		require.NoError(t, err)
+		t.Logf("Received event: ID=%s, Event=%s, Data=%s\n", event.ID, event.Event, string(event.Data))
+		if event.Event == "restarting" {
+			break
+		}
+	}
+
+}
+
+func waitForSSEevent(t *testing.T, url, name, status string) bool {
+	t.Helper()
+
+	appo := false
+
+	itr := NewSSEClient(t.Context(), "GET", url)
+	for event, err := range itr {
+		if err != nil {
+			require.NoError(t, err)
+			fmt.Println("Error receiving event:", err)
+		}
+		t.Logf("Received event: ID=%s, Event=%s, Data=%s\n", event.ID, event.Event, string(event.Data))
+		if event.Event == "app" && event.Data != nil && strings.Contains(string(event.Data), fmt.Sprintf(`"status":"%s"`, status)) && strings.Contains(string(event.Data), fmt.Sprintf(`"name":"%s"`, name)) {
+			appo = true
+			break
+		}
+	}
+
+	return appo
+
 }
